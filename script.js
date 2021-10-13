3cf5fc6e1fc7cd8848112965970ac59bdd1865c4
Harry-Yates <hyates1@gmail.com>
2021-10-13 16:30:53 +0200

    added functionality and synced with media player files

--- script.js
+++ script.js
@@ -1,16 +1,20 @@
-const musicContainer = document.getElementById('music-container');
-const playBtn = document.getElementById('play');
-const prevBtn = document.getElementById('prev');
-const nextBtn = document.getElementById('next');
+// Music related
+const musicContainer = document.getElementById("music-container");
+const playBtn = document.getElementById("play");
+const prevBtn = document.getElementById("prev");
+const nextBtn = document.getElementById("next");
 
-const audio = document.getElementById('audio');
-const progress = document.getElementById('progress');
-const progressContainer = document.getElementById('progress-container');
-const title = document.getElementById('title');
-const cover = document.getElementById('cover');
+const audio = document.getElementById("audio");
+const progress = document.getElementById("progress");
+const progressContainer = document.getElementById("progress-container");
+const title = document.getElementById("title");
+const cover = document.getElementById("cover");
+
+//Space related
+const info = document.querySelector(".info-box");
 
 // Song titles
-const songs = ['hey', 'summer', 'ukulele'];
+const songs = ["hey", "summer", "ukulele"];
 
 // Keep track of song
 let songIndex = 2;
@@ -27,18 +31,18 @@ function loadSong(song) {
 
 // Play song
 function playSong() {
-  musicContainer.classList.add('play');
-  playBtn.querySelector('i.fas').classList.remove('fa-play');
-  playBtn.querySelector('i.fas').classList.add('fa-pause');
+  musicContainer.classList.add("play");
+  playBtn.querySelector("i.fas").classList.remove("fa-play");
+  playBtn.querySelector("i.fas").classList.add("fa-pause");
 
   audio.play();
 }
 
 // Pause song
 function pauseSong() {
-  musicContainer.classList.remove('play');
-  playBtn.querySelector('i.fas').classList.add('fa-play');
-  playBtn.querySelector('i.fas').classList.remove('fa-pause');
+  musicContainer.classList.remove("play");
+  playBtn.querySelector("i.fas").classList.add("fa-play");
+  playBtn.querySelector("i.fas").classList.remove("fa-pause");
 
   audio.pause();
 }
@@ -86,8 +90,17 @@ function setProgress(e) {
 }
 
 // Event listeners
-playBtn.addEventListener('click', () => {
-  const isPlaying = musicContainer.classList.contains('play');
+playBtn.addEventListener("onload", () => {
+  const isPlaying = musicContainer.classList.contains("play");
+
+  if (isPlaying) {
+    pauseSong();
+  } else {
+    playSong();
+  }
+});
+playBtn.addEventListener("click", () => {
+  const isPlaying = musicContainer.classList.contains("play");
 
   if (isPlaying) {
     pauseSong();
@@ -97,14 +110,114 @@ playBtn.addEventListener('click', () => {
 });
 
 // Change song
-prevBtn.addEventListener('click', prevSong);
-nextBtn.addEventListener('click', nextSong);
+prevBtn.addEventListener("click", prevSong);
+nextBtn.addEventListener("click", nextSong);
 
 // Time/song update
-audio.addEventListener('timeupdate', updateProgress);
+audio.addEventListener("timeupdate", updateProgress);
 
 // Click on progress bar
-progressContainer.addEventListener('click', setProgress);
+progressContainer.addEventListener("click", setProgress);
 
 // Song ends
-audio.addEventListener('ended', nextSong);
+audio.addEventListener("ended", nextSong);
+
+// Fetch apis
+const locIqKey = "pk.bb10b56f6e68b7f09bcfdf9e751977b9";
+let storedLocation;
+
+setInterval(() => {
+  fetch("http://api.open-notify.org/iss-now.json")
+    .then((res) => res.json())
+    .then((data) => {
+      let lat = data.iss_position.latitude;
+      let long = data.iss_position.longitude;
+
+      console.log("lat: ", lat);
+      console.log("long: ", long);
+
+      fetch(`https://us1.locationiq.com/v1/reverse.php?key=${locIqKey}&lat=${lat}&lon=${long}&zoom=3&format=json`)
+        .then((res) => res.json())
+        .then((data) => {
+          if (data.error) {
+            // console.log("above the ocean");
+            let currentLocation = generateRandomLocation();
+            if (storedLocation != null) {
+              if (storedLocation != currentLocation) {
+                nextSong();
+                storedLocation = currentLocation;
+              }
+            } else {
+              storedLocation = currentLocation;
+            }
+            postDataCard(lat, long, currentLocation);
+          } else {
+            nextSong();
+            let country = data.address.country;
+            postDataCard(lat, long, country);
+            console.log("Country: ", data.address.country);
+          }
+        });
+    });
+
+  fetch("http://api.open-notify.org/astros.json")
+    .then((res) => res.json())
+    .then((data) => {
+      let people = data.number;
+      console.log("ppl in space: ", people);
+    });
+}, 1000);
+
+function postDataCard(lat, long, country) {
+  let time = new Date();
+  let hours = time.getHours();
+  let minutes = time.getMinutes();
+  let seconds = time.getSeconds();
+  let formattedTime = `${hours}:${minutes}:${seconds}`;
+  info.innerHTML = `
+  <h4>Time: </h4>
+  <p>${formattedTime}</p>
+  <h4>Latidude: </h4>
+  <p>${lat}</p>
+  <h4>Longitude: </h4>
+  <p>${long}</p>
+  <h4>Location: ${country} </h4>
+  `;
+}
+
+function generateRandomLocation() {
+  let number = generateRandomNumber(30);
+  if (number < 25) {
+    console.log(number);
+    return "Above the ocean";
+  } else if (number === 25) {
+    console.log(number);
+    return "Above a fish";
+  } else if (number === 26) {
+    console.log(number);
+    return "Above a ship";
+  } else if (number === 27) {
+    console.log(number);
+    return "Above a pirate";
+  } else if (number === 28) {
+    console.log(number);
+    return "Above a whale";
+  } else if (number === 29) {
+    console.log(number);
+    return "Above lost person at sea";
+  } else if (number === 30) {
+    console.log(number);
+    return "Above nuclear submarine";
+  }
+}
+
+function generateRandomNumber(max) {
+  return (randomNumber = Math.floor(Math.random() * max));
+}
+
+// create storedLocation
+// get current location
+// check if we have anything in stored location
+// if not set storedLocation
+// else check if they are equal
+// if they are not equal change song
